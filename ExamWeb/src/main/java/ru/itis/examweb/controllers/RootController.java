399bc06c99ddd325356f1683d51cda9b42857058
Juan-Camilo-Suarez <juancamilosuarez3@gmail.com>
2022-06-24 18:02:26 +0300

    login

--- ExamWeb/src/main/java/ru/itis/examweb/controllers/RootController.java
+++ ExamWeb/src/main/java/ru/itis/examweb/controllers/RootController.java
@@ -1,4 +1,14 @@
 package ru.itis.examweb.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class RootController {
+    @GetMapping("/")
+    public String getIndex(Model model) {
+
+        return "root";
+    }
 }
